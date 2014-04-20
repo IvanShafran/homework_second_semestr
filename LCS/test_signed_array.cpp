2e8d9955ad50c4d92713816b0c8b7206103d605f
Alexander G. Pronchenkov <apronchenkov@users.noreply.github.com>
2014-04-20 21:57:52 +0400

    Update test_signed_array.cpp

--- LCS/test_signed_array.cpp
+++ LCS/test_signed_array.cpp
@@ -7,6 +7,8 @@ void TestSignedArray()
     SignedArray<int> signed_array(-1, 2);
     std::vector<int> vector({ 0, 1, 2, 3 });
     signed_array = vector;
+    
+    // @review: Хорошо бы проверить и остальные методы тоже.
 
     if (signed_array[-1] != 0 || signed_array[0] != 1 ||
         signed_array[1] != 2 || signed_array[2] != 3)
